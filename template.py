b1f257afc30d8257ee17821cf369d237c5f2a115
returntoOZ <116332741+returntoOZ@users.noreply.github.com>
2022-11-27 21:03:20 +0900

    Update template.py

--- template.py
+++ template.py
@@ -14,70 +14,70 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 
 def load_dataset(dataset_path):
-    df = pd.read_cvs(dataset_path)
-    return df
+	df = pd.read_cvs(dataset_path)
+	return df
 
 def dataset_stat(dataset_df):
-    #To-Do: Implement this function
-    df1 = dataset_df.drop("target", axis=1)
-    return df1.shape[1], dataset_df.groupby("target").size()[0], dataset_df.groupby("target").size()[1]
+	#To-Do: Implement this function
+	df1 = dataset_df.drop("target", axis=1)
+	return df1.shape[1], dataset_df.groupby("target").size()[0], dataset_df.groupby("target").size()[1]
 
 def split_dataset(dataset_df, testset_size):
-    #To-Do: Implement this function
-    x=dataset_df.drop("target",axis=1)
-    y=dataset_df["target"]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=testset_size)
-    return x_train, x_test, y_train, y_test
+	#To-Do: Implement this function
+	x=dataset_df.drop("target",axis=1)
+	y=dataset_df["target"]
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=testset_size)
+	return x_train, x_test, y_train, y_test
 
 def decision_tree_train_test(x_train, x_test, y_train, y_test):
-    #To-Do: Implement this function
-    dt_cls = DecisionTreeClassifier()
-    dt_cls.fit(x_train, y_train)
-    return accuracy_score(y_test, dt_cls.predict(x_test)) * 100 , precision_score(y_test, dt_cls.predict(x_test)) * 100, recall_score(y_test, dt_cls.predict(x_test)) * 100
+	#To-Do: Implement this function
+	dt_cls = DecisionTreeClassifier()
+	dt_cls.fit(x_train, y_train)
+	return accuracy_score(y_test, dt_cls.predict(x_test)), precision_score(y_test, dt_cls.predict(x_test)), recall_score(y_test, dt_cls.predict(x_test))
 
 
 def random_forest_train_test(x_train, x_test, y_train, y_test):
-    #To-Do: Implement this function
-    rf_cls = RandomForestClassifier()
-    rf_cls.fit(x_train, y_train)
-    return accuracy_score(rf_cls.predict(x_test), y_test) * 100, precision_score(rf_cls.predict(x_test), y_test) * 100, recall_score(rf_cls.predict(x_test), y_test) * 100
+	#To-Do: Implement this function
+	rf_cls = RandomForestClassifier()
+	rf_cls.fit(x_train, y_train)
+	return accuracy_score(rf_cls.predict(x_test), y_test), precision_score(rf_cls.predict(x_test), y_test), recall_score(rf_cls.predict(x_test), y_test)
 
 def svm_train_test(x_train, x_test, y_train, y_test):
-    #To-Do: Implement this function
-    svm_pipe = make_pipeline(
-        StandardScaler(),
-        SVC()
-    )
-    svm_pipe.fit(x_train, y_train)
-    return accuracy_score(y_test, svm_pipe.predict(x_test)) * 100, precision_score(y_test, svm_pipe.predict(x_test)) * 100, recall_score(y_test, svm_pipe.predict(x_test)) * 100
+	#To-Do: Implement this function
+	svm_pipe = make_pipeline(
+		StandardScaler(),
+		SVC()
+	)
+	svm_pipe.fit(x_train, y_train)
+	return accuracy_score(y_test, svm_pipe.predict(x_test)), precision_score(y_test, svm_pipe.predict(x_test)), recall_score(y_test, svm_pipe.predict(x_test))
 
 def print_performances(acc, prec, recall):
-    #Do not modify this function!
-    print ("Accuracy: ", acc)
-    print ("Precision: ", prec)
-    print ("Recall: ", recall)
+	#Do not modify this function!
+	print ("Accuracy: ", acc)
+	print ("Precision: ", prec)
+	print ("Recall: ", recall)
 
 if __name__ == '__main__':
-    #Do not modify the main script!
-    data_path = sys.argv[1]
-    data_df = load_dataset(data_path)
+	#Do not modify the main script!
+	data_path = sys.argv[1]
+	data_df = load_dataset(data_path)
 
-    n_feats, n_class0, n_class1 = dataset_stat(data_df)
-    print ("Number of features: ", n_feats)
-    print ("Number of class 0 data entries: ", n_class0)
-    print ("Number of class 1 data entries: ", n_class1)
+	n_feats, n_class0, n_class1 = dataset_stat(data_df)
+	print ("Number of features: ", n_feats)
+	print ("Number of class 0 data entries: ", n_class0)
+	print ("Number of class 1 data entries: ", n_class1)
 
-    print ("\nSplitting the dataset with the test size of ", float(sys.argv[2]))
-    x_train, x_test, y_train, y_test = split_dataset(data_df, float(sys.argv[2]))
+	print ("\nSplitting the dataset with the test size of ", float(sys.argv[2]))
+	x_train, x_test, y_train, y_test = split_dataset(data_df, float(sys.argv[2]))
 
-    acc, prec, recall = decision_tree_train_test(x_train, x_test, y_train, y_test)
-    print ("\nDecision Tree Performances")
-    print_performances(acc, prec, recall)
+	acc, prec, recall = decision_tree_train_test(x_train, x_test, y_train, y_test)
+	print ("\nDecision Tree Performances")
+	print_performances(acc, prec, recall)
 
-    acc, prec, recall = random_forest_train_test(x_train, x_test, y_train, y_test)
-    print ("\nRandom Forest Performances")
-    print_performances(acc, prec, recall)
+	acc, prec, recall = random_forest_train_test(x_train, x_test, y_train, y_test)
+	print ("\nRandom Forest Performances")
+	print_performances(acc, prec, recall)
 
-    acc, prec, recall = svm_train_test(x_train, x_test, y_train, y_test)
-    print ("\nSVM Performances")
-    print_performances(acc, prec, recall)
+	acc, prec, recall = svm_train_test(x_train, x_test, y_train, y_test)
+	print ("\nSVM Performances")
+	print_performances(acc, prec, recall)
